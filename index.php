0100fb27725372921affe9014fc6113f26ee9fe9
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2023-10-17 17:26:15 +0200

    Ajoute la quatrième modification sur develop

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ echo 'Version initiale de l'application';
 echo 'Première modification sur develop';
 echo 'Seconde modification sur develop';
 echo 'Troisième modification sur develop';
+echo 'Quatrième modification sur develop';

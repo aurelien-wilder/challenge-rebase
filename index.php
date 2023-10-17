1a7fefc5134b6cf93c0d93374ce2749a3e09e662
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2023-10-17 17:25:59 +0200

    Améliore la fonctionnalité

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ echo 'Version initiale de l'application';
 echo 'Première modification sur develop';
 echo 'Seconde modification sur develop';
 echo 'Nouvelle fonctionnalité ajoutée';
+echo 'Amélioration de la fonctionnalité';

936003cfb61791f41694c9977898e81d1b3c6ec0
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2023-10-17 18:29:30 +0200

    Ajoute la troisième modification de another-feature

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ echo 'Troisième modification sur develop';
 echo 'Quatrième modification sur develop';
 echo 'Première modification de another-feature';
 echo 'Seconde modification de another-feature';
+echo 'Troisième modification de another-feature';

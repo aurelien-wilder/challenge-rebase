9dd3de3d6e7ce6348211158cb9c147ec49541cd4
Aurélien Dazy <aurelien.dazy@wildcodeschool.com>
2023-10-17 18:29:15 +0200

    Ajoute la cinquième modification sur develop

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ echo 'Troisième modification sur develop';
 echo 'Quatrième modification sur develop';
 echo 'Nouvelle fonctionnalité ajoutée';
 echo 'Amélioration de la fonctionnalité';
+echo 'Cinquième modification sur develop après la fusion';
